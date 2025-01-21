64abddfc221f1980d9c7566042cf3bc9f1cac394
Aerin Gamaroff <aerin@centbee.com>
2025-01-21 12:48:21 +0200

    ios platform

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.fizzgig.risingmoon',
   appName: 'rising-moon',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;

9e7da82fa2767af8cacabf43470fecada6114b37
Aerin Gamaroff <aerin@centbee.com>
2025-03-14 17:14:07 +0200

    loader component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RestaurantComponent } from './restaurant/restaurant.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [RestaurantComponent],
+  declarations: [RestaurantComponent, LoaderComponent],
   imports: [IonicModule, CommonModule],
-  exports: [RestaurantComponent],
+  exports: [RestaurantComponent, LoaderComponent],
 })
 export class ComponentsModule {}

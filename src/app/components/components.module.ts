45af9c5ce56bd4101843f31de3fa4cf04ff2a08b
Aerin Gamaroff <aerin@centbee.com>
2025-03-17 13:25:15 +0200

    search page and empty screen component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { LoaderComponent } from './loader/loader.component';
+import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
 
 @NgModule({
-  declarations: [RestaurantComponent, LoaderComponent],
+  declarations: [RestaurantComponent, LoaderComponent, EmptyScreenComponent],
   imports: [IonicModule, CommonModule],
-  exports: [RestaurantComponent, LoaderComponent],
+  exports: [RestaurantComponent, LoaderComponent, EmptyScreenComponent],
 })
 export class ComponentsModule {}

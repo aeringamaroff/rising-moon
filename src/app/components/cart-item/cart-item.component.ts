b149ece9b7bd1e02360480780d1d92e096873309
Aerin Gamaroff <aerin@centbee.com>
2025-03-27 09:54:17 +0200

    cart-item component

---
+++ src/app/components/cart-item/cart-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item',
+  templateUrl: './cart-item.component.html',
+  styleUrls: ['./cart-item.component.scss'],
+  standalone: false,
+})
+export class CartItemComponent implements OnInit {
+  @Input() item: any;
+  @Input() index: any;
+  @Output() add: EventEmitter<any> = new EventEmitter();
+  @Output() remove: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  incrementQuantity() {
+    this.add.emit(this.index);
+  }
+
+  decreaseQuantity() {
+    this.remove.emit(this.index);
+  }
+}

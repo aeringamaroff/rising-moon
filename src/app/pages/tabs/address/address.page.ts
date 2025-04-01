daf9c7ed6b804542b6ef952fdb19e26a4a5ee314
Aerin Gamaroff <aerin@centbee.com>
2025-04-01 13:10:20 +0200

    addresses

--- src/app/pages/tabs/address/address.page.ts
+++ src/app/pages/tabs/address/address.page.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class AddressPage implements OnInit {
+  loading: boolean = true;
+  addresses: any[] = [];
+
   constructor() {}
 
-  ngOnInit() {}
+  async ngOnInit() {
+    await this.getAddresses();
+  }
+
+  async getAddresses() {
+    setTimeout(() => {
+      this.addresses = [
+        {
+          title: 'Home',
+          lat: -26.2041,
+          lng: 28.0473,
+          address: '123 Elm Street, Johannesburg',
+          number: '+27 82 123 4567',
+          id: 'ui4fhnkd1',
+        },
+        {
+          title: 'Work',
+          lat: -26.2023,
+          lng: 28.0456,
+          address: '456 Business Ave, Sandton',
+          number: '+27 11 987 6543',
+          id: 'u1200id2',
+        },
+        {
+          title: 'Second House',
+          lat: -26.2075,
+          lng: 28.0501,
+          address: '789 Willow Road, Randburg',
+          number: '+27 72 555 7890',
+          id: 'mjk85uid3',
+        },
+      ];
+
+      this.loading = false;
+    }, 500);
+  }
 
   editAddress() {}
 
